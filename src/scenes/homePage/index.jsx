63a26b0e7180f6f6febbb797fa7410aad2be4e92
Oluwakeji <onabajooluwakeji.daniel@gmail.com>
2023-01-03 18:44:12 +0100

    setup folder and navbar

---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Navbar from 'scenes/navbar'
+
+const index = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  )
+}
+
+export default index
